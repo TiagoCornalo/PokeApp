c5def1a24cef080df2f6d887caaa7ae330623529
tiago cornalo <cornalotiago@gmail.com>
2022-07-05 13:04:27 -0300

    fixing routes

--- api/src/routes/typesRoute.js
+++ api/src/routes/typesRoute.js
@@ -1,6 +1,6 @@
 const typesRouter = require('express').Router()
 const { getTypes } = require('../controllers/types')
 
-typesRouter.get('/types', getTypes)
+typesRouter.get('/api/types', getTypes)
 
 module.exports = typesRouter
